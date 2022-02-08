e0c0b1362bff70c6f5e0d53b899c50a82527b4f5
smsinchanmaitra2007 <smsinchanmaitra2007@gmail.com>
2022-02-08 03:40:41 -0500

    New Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
+import React, {useEffect, useState} from "react"
+import {Layout} from "../components/index"
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function BlogApp({ Component, pageProps }: AppProps) {
+  return <body style={{background: "linear-gradient(90deg, #667eea 0%, #764ba2 100%)"}}>
+    <Layout >
+     <Component {...pageProps} />
+    </Layout>
+  </body>
 }
 
-export default MyApp
+export default BlogApp
