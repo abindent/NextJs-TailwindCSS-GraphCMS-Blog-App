224871833ffa3a678299444068356a33c97ff00f
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-02-08 15:46:29 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,28 +12,28 @@ import { FeaturedPosts } from "../sections"
 
 export default function Home({
     posts
-}) {return ( < div className="container mx-auto">
-    < Head>
-        < title> Avatar Blog < /title>
-                < link rel="icon" href="/favicon.ico" />
-                < /Head>
+}) {return ( <div className="container mx-auto">
+    <Head>
+        <title> Avatar Blog </title>
+                <link rel="icon" href="/favicon.ico" />
+                </Head>
                  <FeaturedPosts  />
-                    < div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-                        < div className="lg:col-span-8 col-span-1"> {
-                            posts.map((post) => ( < div>
-                                < PostCard post={ post.node } key={ post.title } />
-                                < /div>
+                    <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                        <div className="lg:col-span-8 col-span-1"> {
+                            posts.map((post) => ( <div>
+                                <PostCard post={ post.node } key={ post.title } />
+                                </div>
                                     ))
-                                    } < /div>
+                                    } </div>
 
-                                        < div className="lg:col-span-4 col-span-1">
-                                            < div className="lg:sticky relative top-8">
-                                                < PostWidget />
-                                                < Categories />
-                                                < /div>
-                                                    < /div>
-                                                        < /div>
-                                                            < /div>
+                                        <div className="lg:col-span-4 col-span-1">
+                                            <div className="lg:sticky relative top-8">
+                                                <PostWidget />
+                                                <Categories />
+                                                </div>
+                                                    </div>
+                                                        </div>
+                                                            </div>
   )
 }
 
